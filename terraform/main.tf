c4e1d8a4099c8a697b9c30a8cfe9d79d66c8e937
SergeyErshov <s.ershov.shoko@gmail.com>
2022-12-01 10:19:28 +0300

    terrafom fmt maint.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 # Создание ВМ с помощью написанного модуля
 
 module "yandex_instance_1" {
-  source                = "./modules/create_vm"
-  instance_subnet_name  = "a-ru-central1-a"
-  instance_name         = "nginx"
-  zone_name             = "ru-central1-a"
+  source               = "./modules/create_vm"
+  instance_subnet_name = "a-ru-central1-a"
+  instance_name        = "nginx"
+  zone_name            = "ru-central1-a"
 }
 
 /*
